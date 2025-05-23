bd25a72ad0def3981d09a71d863b96f3624bfb7e
tejas274 <shahtejas274@gmail.com>
2025-05-23 09:23:27 -0400

    feat: problem 1 added

--- problem1-180-consecutive-numbers.sql
+++ problem1-180-consecutive-numbers.sql
@@ -15,3 +15,21 @@ JOIN Logs log2 ON log1.id = log2.id - 1
 JOIN Logs log3 ON log2.id = log3.id - 1
 WHERE log1.num = log2.num
   AND log2.num = log3.num
+
+
+--window function
+WITH ThreeConsecutive AS (
+  SELECT
+    CASE
+      WHEN num = LAG(num, 1) OVER (ORDER BY id)
+        AND num = LAG(num, 2) OVER (ORDER BY id)
+      THEN num
+    END AS ConsecutiveNums
+  FROM Logs
+)
+
+SELECT
+    DISTINCT ConsecutiveNums
+FROM ThreeConsecutive
+WHERE
+    ConsecutiveNums IS NOT NULL;
